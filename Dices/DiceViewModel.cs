ed15cb57cd158f3d3f99a422548539c4fbc84c2a
conpawel <pbaszuro@yahoo.pl>
2015-06-09 23:41:26 +0200

    Zadania

---
+++ Dices/DiceViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dices
+{
+    public class DiceViewModel 
+    {
+        private readonly IDispatcher _dispatcher;
+
+        public DiceViewModel(IDispatcher dispatcher)
+        {
+            _dispatcher = dispatcher;
+        }
+
+    }
+}
